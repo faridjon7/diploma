52317976c201435d9a52b547412aea4dbade399a
faridjon7 <farid6566@gmail.com>
2023-07-07 01:52:15 +0300

    Add last urls and buttons

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+from telebot import types
 
 url1 = 'http://fapl.ru'
 r1 = requests.get(url1)
@@ -31,5 +31,53 @@ r5 = requests.get(url5)
 soup5 = BeautifulSoup(r5.content, 'html.parser')
 seria_a_matches = soup5.find_all('div', class_='stat mB15')[0].text
 
+url6 = 'https://www.sports.ru/bundesliga/table/'
+r6 = requests.get(url6)
+soup6 = BeautifulSoup(r6.content, 'html.parser')
+bundesliga_table = soup6.find_all('div', class_='stat mB6')[0].text
+
+url7 = 'https://www.sports.ru/bundesliga/calendar/'
+r7 = requests.get(url7)
+soup7 = BeautifulSoup(r7.content, 'html.parser')
+bundesliga_matches = soup7.find_all('div', class_='stat mB15')[0].text
+
+url8 = 'https://www.sports.ru/ligue-1/table/'
+r8 = requests.get(url8)
+soup8 = BeautifulSoup(r8.content, 'html.parser')
+ligue_1_table = soup8.find_all('div', class_='stat mB6')[0].text
+
+url9 = 'https://www.sports.ru/ligue-1/calendar/'
+r9 = requests.get(url9)
+soup9 = BeautifulSoup(r9.content, 'html.parser')
+ligue_1_matches = soup9.find_all('div', class_='stat mB15')[0].text
+
+url10 = 'https://terrikon.com/champions-league'
+r10 = requests.get(url10)
+soup10 = BeautifulSoup(r10.content, 'html.parser')
+ucl = soup10.find_all('div', class_='groups-info')[0].text
+
+url11 = 'https://terrikon.com/europa-league'
+r11 = requests.get(url11)
+soup11 = BeautifulSoup(r11.content, 'html.parser')
+uel = soup11.find_all('div', class_='groups-info')[0].text
+
+url12 = 'https://terrikon.com/conference-league'
+r12 = requests.get(url12)
+soup12 = BeautifulSoup(r12.content, 'html.parser')
+conference_league = soup12.find_all('div', class_='groups-info')[0].text
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('Лига Чемпионов')
+    button2 = types.KeyboardButton('Лига Европы')
+    button3 = types.KeyboardButton('Лига Конференций')
+    button4 = types.KeyboardButton('АПЛ')
+    button5 = types.KeyboardButton('ЛаЛига')
+    button6 = types.KeyboardButton('Серия А')
+    button7 = types.KeyboardButton('Бундеслига')
+    button8 = types.KeyboardButton('Лига 1')
+    markup.add(button1, button2, button3, button4, button5, button6, button7, button8)
 
 
